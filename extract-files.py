c25397a2bc0757a8ddaaf1804c914f7ca5ff776a
Mashopy <eliasgheeraert@gmail.com>
2023-04-10 19:48:00 +0200

    sm8250-common: Patch camera.qcom.so instead of com.qti.chi.override.so
    It seems that realme rely over camera.qcom.so for auxiliary camera availability.
    
    Test: Open CameraIDs app and all camera IDs are now showed.
          Open a custom Google Camera app and manually switch to each cameras.
    
    Change-Id: I6aa269a41bdc81405137bf6d1d81705470982488

--- extract-files.py
+++ extract-files.py
@@ -119,9 +119,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
-    'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
-        .add_needed('libcamera_metadata_shim.so')
-        .binary_regex_replace(b'com.oem.autotest', b'\x00om.oem.autotest'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so'),
     'odm/lib64/libOGLManager.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
