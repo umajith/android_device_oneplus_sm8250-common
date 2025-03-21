1fb8b0bdc9481d6311668360b7161fce7be1b918
Bijoy Sarkar <ryukv009@gmail.com>
2025-03-21 08:37:31 +0000

    sm8250-common: Create vendor camera symlinks

--- common.mk
+++ common.mk
@@ -101,6 +101,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera vendor symlinks
+PRODUCT_PACKAGES += \
+    lib64_com.qti.stats.haf.so_symlink \
+    lib64_com.qti.stats.pdlib.so_symlink \
+    lib64_libipebpsstriping.so_symlink
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
