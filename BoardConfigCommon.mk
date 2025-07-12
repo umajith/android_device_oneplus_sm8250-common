7ff9b175d1de54aa717da5929562bd370699789c
provasishh <iambumba3@gmail.com>
2025-07-12 05:18:18 +0000

    sm8250-common: Update SP from RMX3371_14.0.0.1800(EX01)
    Signed-off-by: provasishh <iambumba3@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,7 +134,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-VENDOR_SECURITY_PATCH := 2024-10-05
+VENDOR_SECURITY_PATCH := 2025-03-01
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
