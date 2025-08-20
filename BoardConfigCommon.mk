5e62914e61812af2478462161f4be77165f1d7d4
Guidix <guidixm@gmail.com>
2025-08-20 04:54:56 +0300

    sm8250-common: Use platform security patch as vendor security patch
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-VENDOR_SECURITY_PATCH := 2025-06-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
